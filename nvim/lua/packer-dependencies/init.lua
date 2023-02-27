a45fbc6a334eb1e30953a227c7f2e6a561b25e2f
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-02-27 08:41:56 -0500

    refactor: removing plugin compile file

--- nvim/lua/packer-dependencies/init.lua
+++ nvim/lua/packer-dependencies/init.lua
@@ -20,13 +20,4 @@ return require('packer').startup(function()
         requires = { 'kyaxdani42/nvim-web-devicons', opt=true}
     }
  
---[[    use {
-        'nvim-neo-tree/neo-tree.nvim',
-        branch = 'v2.x',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-tree/nvim-web-devicons',
-            'MunifTanjim/nui.nvim'
-        }
-    }]]--
 end)
