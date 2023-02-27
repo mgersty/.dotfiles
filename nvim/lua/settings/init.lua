08a980a573771fa8a70bde56936c85208911157f
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-02-27 14:36:58 -0500

    saving_progress: changing colorscheme

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -23,10 +23,10 @@ set.cursorline = true
 
 
 
-vim.cmd("colorscheme nordfox")
---[[vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.cmd("colorscheme carbonfox")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-]]--
+
 vim.notify = require("notify")
 
 
