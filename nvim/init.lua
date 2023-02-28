454355d41487ff95a55c6aa474cb5e5b629a1fcd
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-02-27 23:13:43 -0500

    saving_progress: have autocompletions workin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 require('settings')
-require('packer-dependencies')
+require('dependencies')
 require('plugins')
 require('plugins.lsp')
 require('mappings')
--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,3 +1,55 @@
+
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+cmp.setup({
+    	formatting = {
+		format = lspkind.cmp_format({
+			with_text = true, -- do not show text alongside icons
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
+
+	mapping = {
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end,
+		["<S-Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end,
+	},
+    sources={
+        { name = "nvim_lsp" }
+    }
+})
+
+
+
+
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
