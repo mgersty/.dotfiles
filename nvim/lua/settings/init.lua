f4f970464fe762b9013d27ac8728fdc21086181e
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-03-09 10:17:15 -0500

    refactor: removing transparent background

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -24,8 +24,8 @@ set.cursorline = true
 
 
 vim.cmd("colorscheme carbonfox")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.notify = require("notify")
 
