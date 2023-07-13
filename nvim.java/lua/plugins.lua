81723a00a3d37ffda1340ffa7ba7a04f8310f779
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-07-13 10:04:58 -0400

    refactor: adding in mason to handle the lang-server packages vs brew/apt

--- nvim.java/lua/plugins.lua
+++ nvim.java/lua/plugins.lua
@@ -1,7 +1,10 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
+  use {
+    "williamboman/mason.nvim",
+    run = ":MasonUpdate" -- :MasonUpdate updates registry contents
+}
   use 'mfussenegger/nvim-dap'
   use 'mfussenegger/nvim-jdtls'
   use 'nvim-lua/plenary.nvim'
