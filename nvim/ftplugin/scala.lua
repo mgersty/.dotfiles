468144c53349dd87a4c7f0ff7d0cd8cf8ae69659
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-12-03 13:19:11 -0500

    Adding in functioning scala metals setup

--- nvim/ftplugin/scala.lua
+++ nvim/ftplugin/scala.lua
@@ -1,1 +1,14 @@
-print("hey look its a scala page")
+local metals = require("metals")
+local metals_configs = metals.bare_config()
+
+
+metals_configs.settings = {
+    showImplicitArguments = true,
+    excludedPackages = { "akka.actor.typed.javadsl" }
+}
+
+metals_configs.init_options.statusBarProvider="on"
+metals_configs.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+metals.setup_dap()
+metals.initialize_or_attach(metals_configs)
