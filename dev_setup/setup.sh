8e72e2660063be1e32b5e0b344171442151641fe
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-12-12 15:23:08 -0500

    updating setup to do a proper install

--- dev_setup/setup.sh
+++ dev_setup/setup.sh
@@ -1,42 +1,73 @@
 #!/bin/bash
-
-
-
-
+#
 function main(){
     echo "Installing apps from debian package manager"
-    
-    apt update && apt upgrade -y
-    apt install sudo -y
 
-    sudo apt install \
-        ncdu \
+    apt update && apt upgrade -y
+    echo "Checking for sudo program"
+    if ! command -v sudo &> /dev/null
+    then
+        echo "Sudo is not present.  Installing now"
+        apt install -y --no-install-recommends sudo
+    fi
+    sudo apt install -y --no-install-recommends \
+        sudo \
+        ca-certificates \
         build-essential \
+        gnupg \
+        gzip \
+        curl \
+        unzip \
+        wget
+
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+
+    sudo apt update
+
+    sudo apt install -y --no-install-recommends \
+        zsh \
+        jq \
+        ncdu \
         htop \
         tmux \
         tree \
-        curl \
-        unzip \
-        wget \
         neovim \
         git \
-        maven -y
-    
+        openjdk-8-jdk \
+        scala \
+        maven \
+        gh \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io
+
+
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+
     echo "Installing oh my zsh shell"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
-    
+
     echo "Installing aws cli"
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
         && unzip awscliv2.zip \
         && sudo ./aws/install
 
-    echo "Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#     echo "Installing Homebrew"
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-
-    echo "Installing Pyenv"
-    brew install pyenv
+#     echo "Installing Pyenv"
+#     brew install pyenv
+#     brew install glab
 }
-
 
 main
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require("packer").startup(function(use)
 	-- Dependency Management
 	use("wbthomason/packer.nvim")
@@ -72,4 +85,11 @@ return require("packer").startup(function(use)
 	use("christoomey/vim-tmux-navigator")
     use("dusans/vim-hardmode")
     use("simrat39/symbols-outline.nvim")
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
+
+
+
