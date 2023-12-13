54f148a01d241bddfe2853539c2418ec5d9667ef
Matthew Gerstemeier <mgerstemeier@cas.org>
2023-12-12 22:25:22 -0500

    testing

--- dev_setup/setup.sh
+++ dev_setup/setup.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-export NEOVIM_VERSION='0.9.4'
-export DELTA_VERION='0.16.5'
+set -e
 
-echo "Installing apps from debian package manager"
+NEOVIM_VERSION='0.9.4'
+DELTA_VERION='0.16.5'
+
+WORK_DIR=$(mktemp -d)
+cd "${WORK_DIR}"
+echo "Switching to ${PWD} as current working directory"
 
 apt update && apt upgrade -y
-echo "Checking for sudo program"
+
+#In case the environment this script is running in does not have sudo installed
 if ! command -v sudo &> /dev/null
 then
     echo "Sudo is not present.  Installing now"
     apt install -y --no-install-recommends sudo
 fi
+
+########################################
+########################################
+#Installing essential dev enviroment utils
+########################################
+########################################
 sudo apt install -y --no-install-recommends \
     sudo \
     ca-certificates \
@@ -23,19 +34,31 @@ sudo apt install -y --no-install-recommends \
     wget \
     vim
 
+########################################
+########################################
+#Setting up external sources for apt
+########################################
+########################################
+
+# Setup Docker source list entry for apt
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-
+# Setup github cli source list entry
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 
 sudo apt update
 
+########################################
+########################################
+# Installing local developer tools and sdks
+########################################
+########################################
 sudo apt install -y --no-install-recommends \
     zsh \
     jq \
@@ -47,14 +70,8 @@ sudo apt install -y --no-install-recommends \
     openjdk-8-jdk \
     scala \
     maven \
-    gh \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io
+    gh
 
-
-sudo groupadd docker
-sudo usermod -aG docker "$USER"
 
 echo "Installing oh my zsh shell"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
@@ -67,8 +84,36 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 wget "https://github.com/dandavison/delta/releases/download/0.16.5/git-delta_${DELTA_VERSION}_amd64.deb"
 sudo dpkg -i git-delta_${DELTA_VERION}_amd64.deb
 
+
+########################################
+########################################
+# Installing Docker
+########################################
+########################################
+sudo apt install -y --no-intstall-recommends \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+&& sudo groupadd docker \
+&& sudo usermod -aG docker "$USER"
+
+
+########################################
+########################################
+# Setting up Neovim
+########################################
+########################################
 wget "https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux64.tar.gz"
 sudo tar -xvzf nvim-linux64.tar.gz -C /usr/local
 sudo ln -s /usr/local/nvim-linux64/bin/nvim /usr/local/bin/nvim
+
+git checkout https://github.com/mgersty/.dotfiles.git && sudo mv .dotfiles "${HOME}"
+DOT_FILES_LOCATION="${PWD}/.dotfiles"
+
+mkdir -p "${HOME}"/.config
+ln -s "${DOT_FILES_LOCATION}"/nvim "${HOME}/.config/nvim"
+ln -s "${DOT_FILES_LOCATION}"/.tmux.conf "${HOME}/.tmux.conf"
+ln -s "${DOT_FILES_LOCATION}"/.gitconfig "${HOME}/.gitconfig"
+
 
 
