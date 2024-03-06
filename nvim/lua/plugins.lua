602ad840923a6f157134955cdc103a9d0c8f5193
Matthew Gerstemeier <mgerstemeier@cas.org>
2024-03-05 22:49:41 -0500

    Adding in live mkdown preview

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -92,6 +92,13 @@ use "rafamadriz/friendly-snippets"
 	use("christoomey/vim-tmux-navigator")
     use("dusans/vim-hardmode")
     use("simrat39/symbols-outline.nvim")
+-- install without yarn or npm
+    use({
+        "iamcco/markdown-preview.nvim",
+         run = function() vim.fn["mkdp#util#install"]() end,
+    })
+    -- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 
     if packer_bootstrap then
         require('packer').sync()
