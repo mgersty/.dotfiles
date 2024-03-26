9325e3f9336a0dfc878a482f8328d2acc160d908
Matthew Gerstemeier <mgerstemeier@cas.org>
2024-03-25 22:17:43 -0400

    updating formatting
    <type>[optional scope]: <description>
    
    [optional body]
    
    [optional footer(s)]

--- nvim/ftplugin/scala.lua
+++ nvim/ftplugin/scala.lua
@@ -1,13 +1,12 @@
 local metals = require("metals")
 local metals_configs = metals.bare_config()
 
-
 metals_configs.settings = {
     showImplicitArguments = true,
-    excludedPackages = { "akka.actor.typed.javadsl" }
+    excludedPackages = { "akka.actor.typed.javadsl" },
 }
 
-metals_configs.init_options.statusBarProvider="on"
+metals_configs.init_options.statusBarProvider = "on"
 metals_configs.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 metals.setup_dap()
