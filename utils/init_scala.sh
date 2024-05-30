8c6cc243c544e4b8aa432e7e6afbf1ea0a2152b7
Matthew Gerstemeier <mgerstemeier@cas.org>
2024-05-29 22:46:25 -0400

    fixing output directories for scala and java generation projects

--- utils/init_scala.sh
+++ utils/init_scala.sh
@@ -4,13 +4,14 @@ if [[ -z "${1}" ]]; then
     exit 1;
 fi
 ARTIFACT_ID="${1}"
-echo "Creating Java Project: org.gersty.${ARTIFACT_ID}"
+echo "Creating Scala Project: org.gersty.${ARTIFACT_ID}"
 mvn archetype:generate -B \
     -DarchetypeGroupId=org.gersty \
-    -DarchetypeArtifactId=java11-template \
+    -DarchetypeArtifactId=scala-maven-template \
     -DarchetypeVersion=1.0.0 \
     -DgroupId=org.gersty \
     -DartifactId="${ARTIFACT_ID}" \
     -Dversion=0.1.0-SNAPSHOT \
     -Dpackage=org.gersty.${ARTIFACT_ID} \
     -DoutputDirectory="$(pwd)"
+
