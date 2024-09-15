50da7da18a47961e67820f18ac03f890b510ef9d
Matthew Gerstemeier <mgerstem@gmail.com>
2024-09-15 10:08:35 -0400

    resetting email in gitconfig and updating hyprland configs

---
+++ hyprland/install.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+
+
+sudo pacman -S hyprland \
+    waybar \
+    rofi-wayland \
+    kitty \
+    dunst \
+    thunar \
+    xdg-desktop-portal-hyprland \
+    qt5-wayland \
+    qt6-wayland \
+    hyprpaper \
+    hyprlock \
+    firefox \
+    ttf-font-awesome \
+    fastfetch \
+    ttf-fira-sans \
+    ttf-fira-code \
+    ttf-firacode-nerd \
+    fuse2 \
+    gtk4 \
+    libadwaita \
+    jq \
+    python-gobject
+    --noconfirm
