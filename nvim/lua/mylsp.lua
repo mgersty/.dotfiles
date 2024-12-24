32cfa70f81cdbed091e16f3f782d9c20430119a3
Matthew Gerstemeier <mgerstem@gmail.com>
2024-12-23 21:55:13 -0500

    misc fix

--- nvim/lua/mylsp.lua
+++ nvim/lua/mylsp.lua
@@ -2,9 +2,6 @@
 local client = vim.lsp.start_client {
     name = "educationalsp",
     cmd = { "/home/gersty/sandbox/go.projects/go_lsp/lsp" },
-    on_attach = function ()
-        vim.notify("Fuck you dumbass")
-    end
 }
 
 if not client then
