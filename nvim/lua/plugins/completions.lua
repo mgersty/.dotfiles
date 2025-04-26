dbcd461cdf5ea43de04e35c38d8bf9139778a068
Matthew Gerstemeier <mgerstem@gmail.com>
2025-04-26 18:38:40 -0400

    updating snippet source for blink.cmp

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,10 +1,12 @@
 return {
     "saghen/blink.cmp",
-    dependencies = "rafamadriz/friendly-snippets",
-    version = "v0.*",
+    dependencies = "L3MON4D3/LuaSnip",
+    version = "v1.*",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
         keymap = { preset = "super-tab" },
+        snippets = { preset = "luasnip" },
+        signature = { enabled = true },
     },
 }
