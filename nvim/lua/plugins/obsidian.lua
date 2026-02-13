e642456ed437d73963da059a2056fc3af85e2fef
Matthew Gerstemeier <mgerstemeier@cas.org>
2026-02-13 10:37:02 -0500

    fixing title creation for obsidian removing codediff feature

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -16,5 +16,13 @@ return {
             },
         },
         completion = { nvim_cmp = false, blink = true, create_new=false},
+        note_id_func = function(title)
+            if title ~= nil and title ~= "" then
+                return title
+            else
+                -- Fallback to generated ID if no title provided
+                return require("obsidian.builtin").zettel_id()
+            end
+        end,
     },
 }
