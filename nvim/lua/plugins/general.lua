6b92b2f7c764afc4a542c3410c4060036c012581
Matthew Gerstemeier <mgerstemeier@cas.org>
2026-03-05 12:16:31 -0500

    updating ghostty transparency

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -1,95 +1,106 @@
 -- General Plugins
 return {
     -- {
-  -- "esmuellert/codediff.nvim",
-  -- dependencies = { "MunifTanjim/nui.nvim" },
-  -- cmd = "CodeDiff",
--- },
-    { "echasnovski/mini.icons" },
-    {
-        "rachartier/tiny-inline-diagnostic.nvim",
-        event = "VeryLazy", -- Or `LspAttach`
-        priority = 1000,    -- needs to be loaded in first
-        config = function()
-            require('tiny-inline-diagnostic').setup({
-                preset = "ghost"
-            })
-            vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
-        end
-    },
-    {
-        "hedyhli/outline.nvim",
-        lazy = true,
-        cmd = { "Outline", "OutlineOpen" },
-        keys = { -- Example mapping to toggle outline
-            { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    --     'ggml-org/llama.vim',
+    --     init = function()
+    --         vim.g.llama_config = {
+    --             endpoint_fim =           '',
+    --             endpoint_inst =          'http://macarch:8012/v1/chat/completions',
+    --             auto_fim=false
+		    -- }
+
+    --     end
+    -- },
+    -- {
+        -- "esmuellert/codediff.nvim",
+        -- dependencies = { "MunifTanjim/nui.nvim" },
+        -- cmd = "CodeDiff",
+        -- },
+        { "echasnovski/mini.icons" },
+        {
+            "rachartier/tiny-inline-diagnostic.nvim",
+            event = "VeryLazy", -- Or `LspAttach`
+            priority = 1000,    -- needs to be loaded in first
+            config = function()
+                require('tiny-inline-diagnostic').setup({
+                    preset = "ghost"
+                })
+                vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+            end
         },
-        config = function()
-            require("outline").setup({
-                outline_window = {
-                    position = "left",
-                    show_relative_numbers = true,
-                    width = 15,
-                },
-                outline_items = {
-                    show_symbol_details = true,
-                },
-                -- preview_window={
-                --     auto_preview=true
-                -- },
-                symbols = {
-                    -- icon_fetcher = function()
-                    --     return ""
-                    -- end,
-                    icons = {
-                        Enum = { icon = "󰫲", hl = "Type" },
-                        Struct = { icon = "", hl = "Structure" },
-                        Object = { icon = "󰫶", hl = "Type" },
+        {
+            "hedyhli/outline.nvim",
+            lazy = true,
+            cmd = { "Outline", "OutlineOpen" },
+            keys = { -- Example mapping to toggle outline
+                { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+            },
+            config = function()
+                require("outline").setup({
+                    outline_window = {
+                        position = "left",
+                        show_relative_numbers = true,
+                        width = 100,
                     },
+                    outline_items = {
+                        show_symbol_details = true,
+                    },
+                    -- preview_window={
+                        --     auto_preview=true
+                        -- },
+                        symbols = {
+                            -- icon_fetcher = function()
+                                --     return ""
+                                -- end,
+                                icons = {
+                                    Enum = { icon = "󰫲", hl = "Type" },
+                                    Struct = { icon = "", hl = "Structure" },
+                                    Object = { icon = "󰫶", hl = "Type" },
+                                },
+                            },
+                        })
+                    end,
                 },
-            })
-        end,
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
-        config = function()
-            require("ibl").setup({ indent = { char = "." } })
-        end,
-    },
-    { "tpope/vim-commentary" },
-    { "catppuccin/nvim",                 name = "catppuccin" },
-    { "mfussenegger/nvim-jdtls",         dependencies = "mfussenegger/nvim-dap" },
-    { "christoomey/vim-tmux-navigator" },
-    { "arcticicestudio/nord-vim" },
-    { "cocopon/iceberg.vim" },
-    { "olivercederborg/poimandres.nvim" },
-    { "Mofiqul/adwaita.nvim" },
-    { "kyazdani42/blue-moon" },
-    { "kvrohit/substrata.nvim" },
-    { "FrenzyExists/aquarium-vim" },
-    { "Verf/deepwhite.nvim" },
-    { "marko-cerovac/material.nvim" },
-    { "nyoom-engineering/oxocarbon.nvim" },
-    { "gregsexton/Atom" },
-    { "tyrannicaltoucan/vim-deep-space" },
-    { "rose-pine/neovim" },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        "askfiy/visual_studio_code",
-        config = function()
-            require("visual_studio_code").setup({
-                mode = "light",
-            })
-        end,
-    },
-    { "rakr/vim-one" },
-}
+                {
+                    "lukas-reineke/indent-blankline.nvim",
+                    main = "ibl",
+                    ---@module "ibl"
+                    ---@type ibl.config
+                    opts = {},
+                    config = function()
+                        require("ibl").setup({ indent = { char = "." } })
+                    end,
+                },
+                { "tpope/vim-commentary" },
+                { "catppuccin/nvim",                 name = "catppuccin" },
+                { "mfussenegger/nvim-jdtls",         dependencies = "mfussenegger/nvim-dap" },
+                { "christoomey/vim-tmux-navigator" },
+                { "arcticicestudio/nord-vim" },
+                { "cocopon/iceberg.vim" },
+                { "olivercederborg/poimandres.nvim" },
+                { "Mofiqul/adwaita.nvim" },
+                { "kyazdani42/blue-moon" },
+                { "kvrohit/substrata.nvim" },
+                { "FrenzyExists/aquarium-vim" },
+                { "Verf/deepwhite.nvim" },
+                { "marko-cerovac/material.nvim" },
+                { "nyoom-engineering/oxocarbon.nvim" },
+                { "gregsexton/Atom" },
+                { "tyrannicaltoucan/vim-deep-space" },
+                { "rose-pine/neovim" },
+                {
+                    "folke/tokyonight.nvim",
+                    lazy = false,
+                    priority = 1000,
+                    opts = {},
+                },
+                {
+                    "askfiy/visual_studio_code",
+                    config = function()
+                        require("visual_studio_code").setup({
+                            mode = "light",
+                        })
+                    end,
+                },
+                { "rakr/vim-one" },
+            }
