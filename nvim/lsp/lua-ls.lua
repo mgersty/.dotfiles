685b709e02e8fce544ca307bc97eb590c6b4a25b
Matthew Gerstemeier <mgerstemeier@cas.org>
2026-03-07 08:56:01 -0500

    updating general plugins to handle opencode.nvim and typescript-tools nvim

--- nvim/lsp/lua-ls.lua
+++ nvim/lsp/lua-ls.lua
@@ -1,22 +1,31 @@
+
+local root_markers1 = {
+  '.emmyrc.json',
+  '.luarc.json',
+  '.luarc.jsonc',
+}
+local root_markers2 = {
+  '.luacheckrc',
+  '.stylua.toml',
+  'stylua.toml',
+  'selene.toml',
+  'selene.yml',
+}
+
+---@type vim.lsp.Config
 return {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2, { '.git' } }
+    or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { '.git' }),
+  settings = {
+    Lua = {
+      codeLens = { enable = true },
+      hint = { enable = true, semicolon = 'Disable' },
+      diagnostics = {
+          globals = {'vim'}
 
-    root_markers = {
-        '.luarc.json',
-        '.luarc.jsonc',
-        '.luacheckrc',
-        '.stylua.toml',
-        'stylua.toml',
-        'selene.toml',
-        'selene.yml',
+      }
     },
-
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-        },
-    },
+  },
 }
--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -5,23 +5,80 @@ return {
         dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
         opts = {},
     },
+
+    { "nickjvandyke/opencode.nvim",
+        version = "*", -- Latest stable release
+        dependencies = {
+            {
+                -- `snacks.nvim` integration is recommended, but optional
+                ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+                "folke/snacks.nvim",
+                optional = true,
+                opts = {
+                    input = {}, -- Enhances `ask()`
+                    picker = { -- Enhances `select()`
+                        actions = {
+                            opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
+                        },
+                        win = {
+                            input = {
+                                keys = {
+                                    ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        config = function()
+            ---@type opencode.Opts
+            vim.g.opencode_opts = {
+                -- Your configuration, if any; goto definition on the type or field for details
+            }
+
+            vim.o.autoread = true -- Required for `opts.events.reload`
+
+            -- Recommended/example keymaps
+            vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end,
+                { desc = "Ask opencode…" })
+            vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
+                { desc = "Execute opencode action…" })
+            vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,
+                { desc = "Toggle opencode" })
+
+            vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
+                { desc = "Add range to opencode", expr = true })
+            vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end,
+                { desc = "Add line to opencode", expr = true })
+
+            vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,
+                { desc = "Scroll opencode up" })
+            vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end,
+                { desc = "Scroll opencode down" })
+
+            -- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
+            vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+            vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+        end,
+    },
     { "echasnovski/mini.icons" },
     {
         "rachartier/tiny-inline-diagnostic.nvim",
-        event = "VeryLazy",     -- Or `LspAttach`
-        priority = 1000,        -- needs to be loaded in first
+        event = "VeryLazy", -- Or `LspAttach`
+        priority = 1000,    -- needs to be loaded in first
         config = function()
             require('tiny-inline-diagnostic').setup({
                 preset = "ghost"
             })
-            vim.diagnostic.config({ virtual_text = false })     -- Only if needed in your configuration, if you already have native LSP diagnostics
+            vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
         end
     },
     {
         "hedyhli/outline.nvim",
         lazy = true,
         cmd = { "Outline", "OutlineOpen" },
-        keys = {     -- Example mapping to toggle outline
+        keys = { -- Example mapping to toggle outline
             { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
         },
         config = function()
